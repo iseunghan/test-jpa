84751174575262f4f1c70185878c7ea270261992
iseunghan <tmdgks301@naver.com>
2022-03-30 13:56:45 +0900

    [2] Join Fetch 사용

--- src/main/java/me/iseunghan/testjpa/domain/TeamRepository.java
+++ src/main/java/me/iseunghan/testjpa/domain/TeamRepository.java
@@ -1,8 +1,13 @@
 package me.iseunghan.testjpa.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    @Query(value = "select t from Team t join fetch t.members")
+    List<Team> findAllJoinFetch();
 }
--- src/main/java/me/iseunghan/testjpa/domain/TeamService.java
+++ src/main/java/me/iseunghan/testjpa/domain/TeamService.java
@@ -2,7 +2,6 @@ package me.iseunghan.testjpa.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,14 +15,8 @@ public class TeamService {
         return teamRepository.save(team);
     }
 
-    /**
-     * @Transactional을 붙이게 되면, TeamService.findAll 메소드 이름으로 트랜잭션이 생성된다.
-     * SimpleJpaRepository에 붙은 @Transactional의 기본속성으로 인해 이전에 생성된 트랜잭션에 참여하게 되므로,
-     * TeamService.findAll 까지 트랜잭션이 살아있어 LazyLoading이 가능하다.
-     */
-    @Transactional(readOnly = true)
     public List<Team> findAll() {
-        List<Team> teams = teamRepository.findAll();
+        List<Team> teams = teamRepository.findAllJoinFetch();
         extractMembers(teams);
         return teams;
     }
