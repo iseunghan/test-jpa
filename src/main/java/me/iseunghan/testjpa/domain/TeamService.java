f469ee3614e1c1c3446f595c6ea1d6191d6cd180
iseunghan <tmdgks301@naver.com>
2022-03-30 13:49:32 +0900

    [1] TeamSerivce.findAll @Transactional 붙이기

--- src/main/java/me/iseunghan/testjpa/domain/TeamService.java
+++ src/main/java/me/iseunghan/testjpa/domain/TeamService.java
@@ -16,6 +16,12 @@ public class TeamService {
         return teamRepository.save(team);
     }
 
+    /**
+     * @Transactional을 붙이게 되면, TeamService.findAll 메소드 이름으로 트랜잭션이 생성된다.
+     * SimpleJpaRepository에 붙은 @Transactional의 기본속성으로 인해 이전에 생성된 트랜잭션에 참여하게 되므로,
+     * TeamService.findAll 까지 트랜잭션이 살아있어 LazyLoading이 가능하다.
+     */
+    @Transactional(readOnly = true)
     public List<Team> findAll() {
         List<Team> teams = teamRepository.findAll();
         extractMembers(teams);
